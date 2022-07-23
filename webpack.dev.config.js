25eea3cdadd6543542ca2488a4e427ee5a6c9f2c
ethanphelps <ethanrphelpsapps@gmail.com>
2022-07-22 23:31:34 -0500

    react hello world page loads

---
+++ webpack.dev.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'index.tsx')
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js",
+    clean: true
+  },
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: "asset",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader' // webpack processes loaders from right to left
+        ]
+      } 
+    ]
+  }
+}
---
+++ webpack.prod.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'index.tsx')
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].[contenthash].js",
+    clean: true
+  },
+  mode: "production",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: "asset",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader' // loaders not included with webpack, so must npm install them
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' // webpack processes loaders from right to left. sass -> css -> js representation / style tags in html
+        ]
+      },
+    ]
+  }
+}
